79b6d8dcfc9b179c2f17edd14b19af7b3503a947
Ishwar Gautam <ishwargautam@lftechnology.com>
2023-03-25 11:29:20 +0545

    Implement User Authentication.

---
+++ src/modules/users/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { ACCESS_TOKEN_SECRET_KEY } from '../../../apiConfig';
+
+const ACCESS_TOKEN: string = ACCESS_TOKEN_SECRET_KEY;
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let token = req.headers.authorization;
+
+    if (token) {
+      token = token.split(' ')[1];
+
+      jwt.verify(token, ACCESS_TOKEN, (err) => {
+        if (!err) {
+          next();
+        } else if (err.message === 'jwt expired') {
+          return res.status(400).json({ message: 'Access Token expired.' });
+        } else {
+          return res.status(500).json({ message: err });
+        }
+      });
+    } else {
+      return res.status(401).json({ message: 'Unauthorized User' });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: 'Unauthorized User' });
+  }
+};
--- src/modules/users/routes/user.route.ts
+++ src/modules/users/routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { auth } from '../middlewares/auth';
 import { validation } from '../middlewares/validation';
 import { get, signup, signin } from '../controllers/user.controller';
 
 const userRouter = express.Router();
 
-userRouter.get('/', get);
+userRouter.get('/', auth, get);
 userRouter.post('/signup', validation, signup);
-userRouter.post('/signin', validation, signin);
+userRouter.post('/signin', auth, validation, signin);
 
 export default userRouter;
