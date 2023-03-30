c653f974d027f7b062b25eaf1fd8ebc18e8ddcf9
Nirajan Bist <bistneon@gmail.com>
2023-03-30 16:52:19 +0545

    UTP-005: Fix dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
+dotenv.config();
 
 import app from './app';
 import { PORT } from './apiConfig';
 import logger from './utils/logger';
 
-dotenv.config();
 const port: number = PORT;
 
 app.listen(port, () => {
