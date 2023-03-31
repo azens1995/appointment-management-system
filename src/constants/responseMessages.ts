6d8f3367bf4dc0476183b1f173dfdc81cded8067
Nirajan Bist <bistneon@gmail.com>
2023-03-31 17:10:46 +0545

    UTP-019: Make constants for response messages

---
+++ src/constants/responseMessages.ts
@@ -0,0 +1,9 @@
+export const GENERIC_BAD_REQUEST_ERROR_MESSAGE = 'something went wrong!';
+export const GENERIC_NOT_FOUND_ERROR_MESSAGE = 'resource not found!';
+export const GENERIC_CONFLICT_ERROR_MESSAGE =
+  "resource conflict's with the existing server state!";
+export const GENERIC_FORBIDDEN_ERROR_MESSAGE = 'unauthorized request!';
+export const GENERIC_SERVER_ERROR_MESSAGE = 'server error!';
+export const GENERIC_OK_RESPONSE_MESSAGE = 'ok';
+export const GENERIC_CREATED_MESSAGE = 'created';
+export const GENERIC_NO_CONTENT_MESSAGE = 'no content';
