6d4cec865cff54129cf14829a19c1940f451e1b7
Eklak <azens1995@gmail.com>
2023-04-04 09:57:04 +0545

    UTP-029: Refactor to rename the variable for result

--- src/common/core/Result.ts
+++ src/common/core/Result.ts
@@ -1,10 +1,12 @@
+import logger from '@utils/logger';
+
 export class Result<T> {
   public isSuccess: boolean;
   public isFailure: boolean;
   public error: T | string;
-  private data: T;
+  private _value: T;
 
-  public constructor(isSuccess: boolean, error?: T | string, data?: T) {
+  public constructor(isSuccess: boolean, error?: T | string, value?: T) {
     if (isSuccess && error) {
       throw new Error(
         'InvalidOperation: A result cannot be successful and contain an error'
@@ -18,20 +20,19 @@ export class Result<T> {
     this.isSuccess = isSuccess;
     this.isFailure = !isSuccess;
     this.error = error;
-    this.data = data;
+    this._value = value;
 
     Object.freeze(this);
   }
 
   public getData(): T {
-    if (!this.isSuccess) {
-      console.log(this.error);
+    if (this.isFailure) {
+      logger.error(this.error);
       throw new Error(
         "Can't get the value of an error result. Use 'errorValue' instead."
       );
     }
-
-    return this.data;
+    return this._value;
   }
 
   public getErrorData(): T {
