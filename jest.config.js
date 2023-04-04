fbecbed98e3687a4602fd2916cc62334011b1147
Nirajan Bist <bistneon@gmail.com>
2023-04-04 14:41:00 +0545

    UTP-025: Setup test config for prisma

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,16 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest'
   },
   testEnvironment: 'node',
-  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
+  testRegex: './tests/.*\\.(test|spec)?\\.(ts|ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/src']
+  roots: ['<rootDir>/tests'],
+  moduleNameMapper: {
+    '^@common/(.*)': '<rootDir>/src/common/$1',
+    '^@modules/(.*)': '<rootDir>/src/modules/$1',
+    '^@utils/(.*)': '<rootDir>/src/utils/$1',
+    '^@config/(.*)': '<rootDir>/src/config/$1',
+    '^client': '<rootDir>/src/client',
+    '^prismaTestSetup': '<rootDir>/src/prismaTestSetup'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/prismaTestSetup.ts']
 };
