000471cc36805662ce264fcbe85fa8237f8c8177
Nirajan Bist <bistneon@gmail.com>
2023-04-04 15:20:37 +0545

    UTP-030: Refactor code to import prisma client

--- src/modules/appointments/repository/appointment.repository.ts
+++ src/modules/appointments/repository/appointment.repository.ts
@@ -1,6 +1,5 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { Prisma } from '@prisma/client';
+import prisma from '@config/client';
 
 // CREATE an appointment
 export async function createAppointment(
