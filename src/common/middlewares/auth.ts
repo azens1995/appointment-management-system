e6ce347c88e8a163938ac153bdc890aa91e53595
Eklak <azens1995@gmail.com>
2023-04-10 14:06:30 +0545

    UTP-027: Refactor auth middleware

--- src/common/middlewares/auth.ts
+++ src/common/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
+import { Result } from '@common/core/Result';
 import { Request, Response, NextFunction } from 'express';
 import { ACCESS_TOKEN_SECRET_KEY } from '@config/appConfig';
+import { AppError, HttpCode } from '@common/exceptions/appError';
 
 const ACCESS_TOKEN: string = ACCESS_TOKEN_SECRET_KEY;
 
@@ -12,22 +14,24 @@ export const auth = (
   try {
     let token = req.headers.authorization;
 
-    if (token) {
-      token = token.split(' ')[1];
-
-      jwt.verify(token, ACCESS_TOKEN, (err, decoded) => {
-        if (!err) {
-          req.user = decoded as Record<string, any>;
-          next();
-        } else if (err.message === 'jwt expired') {
-          return res.status(400).json({ message: 'Access Token expired.' });
-        } else {
-          return res.status(500).json({ message: err });
-        }
-      });
-    } else {
-      return res.status(401).json({ message: 'Unauthorized User' });
+    if (!token) {
+      const error = AppError.unauthorized('Unauthorized User');
+      return res.status(error.httpCode).json(Result.fail(error.message));
     }
+    token = token.split(' ')[1];
+    jwt.verify(token, ACCESS_TOKEN, null, (err, decoded) => {
+      if (!err) {
+        req.user = decoded as Record<string, any>;
+        next();
+      } else if (err.message === 'jwt expired') {
+        const error = AppError.badRequest('Access Token expired.');
+        return res.status(error.httpCode).json(Result.fail(error.message));
+      } else {
+        return res
+          .status(HttpCode.INTERNAL_SERVER_ERROR)
+          .json(Result.fail(err.message));
+      }
+    });
   } catch (error) {
     next(error);
   }
