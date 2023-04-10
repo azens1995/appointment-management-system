faa1ffd98f5a0440a7c161f6fdc1eb04b5535b96
Eklak <azens1995@gmail.com>
2023-04-10 17:52:39 +0545

    Add starter tests folders and files

---
+++ tests/modules/appointments/repository/appointment.repository.spec.ts
@@ -0,0 +1,10 @@
+// Write your tests here
+describe('Name of the group', () => {
+  test('Your first unit test', () => {
+    // Arrange
+    const defaultValue = 2;
+    // Act
+    // Assert
+    expect(2).toBe(defaultValue);
+  });
+});
